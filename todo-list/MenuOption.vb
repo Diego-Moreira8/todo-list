21fd827b88345cdfe49583693a83bfd15d9fddd3
Diego Moreira <diego.moreira529@gmail.com>
2026-06-29 19:20:23 -0300

    feat: adicionado um menu principal onde o usuário pode adicionar e ver tarefas

---
+++ todo-list/MenuOption.vb
@@ -0,0 +1,9 @@
+﻿Public Class MenuOption
+    Public ReadOnly Property Code As Integer
+    Public ReadOnly Property Label As String
+
+    Public Sub New(code As Integer, label As String)
+        Me.Code = code
+        Me.Label = label
+    End Sub
+End Class
--- todo-list/Program.vb
+++ todo-list/Program.vb
@@ -1,13 +1,51 @@
 Imports System
+Imports System.Environment
 
 Module Program
     Sub Main(args As String())
-        Dim newTodoList As TodoList = New TodoList()
+        Dim userTodoList As TodoList = New TodoList()
 
-        newTodoList.AddTodo("Primeira tarefa")
-        newTodoList.AddTodo("Segunda tarefa")
-        'newTodoList.AddTodo("   ")
+        Dim menuOptions As MenuOption() = {
+            New MenuOption(0, "Sair"),
+            New MenuOption(1, "Ver lista"),
+            New MenuOption(2, "Adicionar item")
+        }
+        Dim userOptionInput As Integer = 0
 
-        newTodoList.PrintTasks()
+
+        'Menu principal
+        Console.WriteLine("=====[ Lista de Tarefas ]=====")
+
+        Do
+            Console.WriteLine($"{NewLine}{NewLine}Escolha uma opção no menu digitando o número correspondente e pressionando enter")
+
+            For Each menuOption As MenuOption In menuOptions
+                Console.WriteLine($"[{menuOption.Code}] {menuOption.Label}")
+            Next
+
+            userOptionInput = Console.ReadLine()
+
+            Console.WriteLine()
+
+            Select Case userOptionInput
+                Case 0
+                    Console.WriteLine("Saindo...")
+
+                Case 1
+                    Console.WriteLine(menuOptions(1).Label.ToUpper)
+                    userTodoList.PrintTasks()
+
+                Case 2
+                    Console.WriteLine(menuOptions(2).Label.ToUpper)
+                    Console.WriteLine("Digite uma descrição da tarefa e pressione enter:")
+                    Dim newTodoDescription As String = Console.ReadLine()
+                    userTodoList.AddTodo(newTodoDescription)
+
+                Case Else
+                    Console.WriteLine("Opção inválida, tente novamente.")
+            End Select
+        Loop While userOptionInput <> 0
     End Sub
 End Module
+
+
